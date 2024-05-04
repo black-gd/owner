d095c91ebaae32192d0acabfbaadab4bfd4352d0
black-gd <163583664+black-gd@users.noreply.github.com>
2024-05-04 13:53:41 +0100

    Update owner.js

--- owner.js
+++ owner.js
@@ -1,3 +1,14 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 const { Client } = require('discord.js-selfbot-v13');
 
 const client = new Client({ checkUpdate: false });
